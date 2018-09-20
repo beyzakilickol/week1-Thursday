fb95d2b955eea1bc2a04150c7b92ed5a97e5c598
beyzakilickol <43250495+beyzakilickol@users.noreply.github.com>
2018-09-20 11:47:06 -0500

    assignment

--- exceptions.py
+++ exceptions.py
@@ -17,8 +17,14 @@ while True:
         number1 = int(input("Enter first number: "))
         operand = input('Enter operand (+, *, /, -): ')
         number2 = int(input("Enter second number: "))
-        calculator()
+        choice = input('Press q to quit the program or enter to contunie: ')
+
+        if (choice == 'q'):
+
+            break
+
+        else:
+            print(calculator())
 
     except ValueError:
         print('Enter a valid number')
-        break
